edf2830b0134055a5fe03a4f68eb40ac6f42ee3f
Alex Nielsen <alex.tommy.nielsen@gmail.com>
2023-04-20 20:39:16 -0400

    C# Prep programs completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int grade;
+        string letter;
+
+        Console.Write("What grade did you receive? ");
+        grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+        if (letter == "A" || letter == "B" || letter == "C")
+        {
+            Console.WriteLine("Congrats! You passed the class!");
+        }
+        else 
+        {
+            Console.WriteLine("You did not pass the class.");
+        }
     }
 }
