b825c2c9389c1edcfa95bb62e5eb752407067ac1
Alex Nielsen <alex.tommy.nielsen@gmail.com>
2023-05-13 23:20:05 -0400

    Initial progress on abstraction assignment

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,9 @@
+public class Entry
+{
+    public DateTime _date;
+    public string _prompt;
+    public string _response;
+    public Entry()
+    {
+    }
+}
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,6 @@
+public class PromptGenerator
+{
+    public PromptGenerator()
+    {
+    }
+}
