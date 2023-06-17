884c34fab8193e7744bc5dbefe95f38a3df16acd
Alex Nielsen <alex.tommy.nielsen@gmail.com>
2023-06-17 00:21:27 -0400

    Menu begin dev

---
+++ prove/Develop05/Menu.cs
@@ -0,0 +1,31 @@
+public class Menu
+{
+    private string _fileName;
+    private int _points = 0;
+
+    public Menu()
+    {
+    }
+
+    public string File()
+    {
+        return _fileName;
+    }
+
+    public void StartMenu()
+    {
+        Console.WriteLine($"Points: {_points}");
+        Console.WriteLine("");
+        Console.WriteLine("Menu Options: ");
+        Console.WriteLine(" 1. Create New Goal");
+        Console.WriteLine(" 2. List Goals");
+        Console.WriteLine(" 3. Save Goals");
+        Console.WriteLine(" 4. Load Goals");
+        Console.WriteLine(" 5. Record Event");
+        Console.WriteLine(" 6. Quit");
+    }
+        public int GetPoints()
+    {
+        return _points;
+    }
+}
