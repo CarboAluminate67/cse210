e518d5d221aba98f6653b0024d6d479bf98f47e1
Alex Nielsen <alex.tommy.nielsen@gmail.com>
2023-06-18 19:59:19 -0400

    Begin Goal class

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,4 +1,16 @@
 public abstract class Goal
 {
+    private bool _completeStatus = false;
+    private int _pointValue;
+    private string _name;
+    private string _description;
+
+    public Goal(int pointVal, string name, string des)
+    {
+        _pointValue = pointVal;
+        _name = name;
+        _description = des;
+    }
+
     
 }
